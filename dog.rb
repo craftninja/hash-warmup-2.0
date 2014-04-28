fcd0f48972d0ead4dc053e2ea6214116f43caa21
EmilyPlatzer <emilyplatzer@gmail.com>
2014-04-28 09:31:56 -0600

    returns Harleigh's fav toy and Trixie's fav food (test 02)

--- dog.rb
+++ dog.rb
@@ -1,5 +1,8 @@
 DOG = {
   :cattle_dog => {:names => ['Harleigh', 'brown']},
-  "harleigh" => {:information => {:sport => ["frisbee", "hiking"]}},
-  "trixie" => {:information => {:sport => [:swimming, :prancing]}},
-}
+  "harleigh" => {:information => {:sport => ["frisbee", "hiking"]},
+                 :preferences => [:favorite_toy => "frog"]},
+  "trixie" => {:information => {:sport => [:swimming, :prancing]},
+               :preferences => [:favorite_food => "steak"]},
+
+  }
