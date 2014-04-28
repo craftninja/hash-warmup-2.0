e45cb62277f190ce8e999c02ab23ba0b32dadf43
EmilyPlatzer <emilyplatzer@gmail.com>
2014-04-28 09:53:14 -0600

    returns the dog name from the hash (test 00)

--- dog.rb
+++ dog.rb
@@ -1,5 +1,6 @@
 DOG = {
-  :cattle_dog => {:names => ['Harleigh', 'brown']},
+  :cattle_dog => {:names => ['Harleigh', 'brown'],
+                  :preferences => [:name => 'Harleigh']},
   "harleigh" => {:information => {:sport => ["frisbee", "hiking"]},
                  :preferences => [:favorite_toy => "frog"]},
   "trixie" => {:information => {:sport => [:swimming, :prancing]},
