308bc09974ee16ff4a6bde4ca353ee1cd2b1b3dd
EmilyPlatzer <emilyplatzer@gmail.com>
2014-04-28 09:23:07 -0600

    Returns Harleigh and Trixie's favorite sports (test 03)

--- dog.rb
+++ dog.rb
@@ -1,3 +1,5 @@
 DOG = {
-  :cattle_dog => {:names => ['Harleigh', 'brown']}
+  :cattle_dog => {:names => ['Harleigh', 'brown']},
+  "harleigh" => {:information => {:sport => ["frisbee", "hiking"]}},
+  "trixie" => {:information => {:sport => [:swimming, :prancing]}},
 }
